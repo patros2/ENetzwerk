76bd57e612b0afa2ac4112fdf4187e266088d2a5
patros2 <patrick.rezmer@student.htw-berlin.de>
2016-03-16 14:18:58 +0100

    update

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@
 #include "D:\ENetzwerk\\lib\NodesEdges\Edge.h"
 #include "D:\ENetzwerk\\lib\NodesEdges\Graph.h"
 
-Node* a = new Node("Spannungsquelle", 1.9, 1);
-Node* b = new Node("Kondesator", 3.5, 0);
-Node* c = new Node("Spule", 1.5, 2);
-Node* d = new Node("Widerstand", 2.0, 3);
+Node* a = new Node("Spannungsquelle", 1.9, 4);
+Node* b = new Node("Kondesator", 3.5, 2);
+Node* c = new Node("Spule", 1.5, 3);
+Node* d = new Node("Widerstand", 2.0, 1);
 double wert = 0;
 double test = 0;
 int z = 0;
@@ -25,7 +25,6 @@ double berechnen(Node* a, Node* b, Node* c, Node* d){
 
     for (int i=0;i<2;i++)
         {
-
         for (int j=0;j<2;j++)
             {
                 z=z+1;
@@ -33,23 +32,23 @@ double berechnen(Node* a, Node* b, Node* c, Node* d){
                 name = masche[i][j]->getID();
                 printf("Lauf: %d, %d, %d, %f \n",z,i,j,test);
                 std::cout << name << std::endl;
-            return wert;
+
             }
         }
+    return wert;
 }
 int main(void)
 {
 
 
     berechnen(a,b,c,d);
-
     //printf("%f\n",berechnen(1,2,3,4));
 
     printf("1\n");
-    std::cout << a->getID() << " vom Typ " << a->getType() << " hat den Wert " << a->getValue() << std::endl;
-    std::cout << b->getID() << " vom Typ " << b->getType() << " hat den Wert " << b->getValue() << std::endl;
-    std::cout << c->getID() << " vom Typ " << c->getType() << " hat den Wert " << c->getValue() << std::endl;
-    std::cout << d->getID() << " vom Typ " << d->getType() << " hat den Wert " << d->getValue() << std::endl;
+    std::cout << a->getID() << " vom Typ " << a->getType() << " hat den Wert " << a->getValue() << " und ist ein/e " << a->getName() << std::endl;
+    std::cout << b->getID() << " vom Typ " << b->getType() << " hat den Wert " << b->getValue() << " und ist ein/e " << b->getName() << std::endl;
+    std::cout << c->getID() << " vom Typ " << c->getType() << " hat den Wert " << c->getValue() << " und ist ein/e " << c->getName() << std::endl;
+    std::cout << d->getID() << " vom Typ " << d->getType() << " hat den Wert " << d->getValue() << " und ist ein/e " << d->getName() << std::endl;
 
     return 0;
 }
