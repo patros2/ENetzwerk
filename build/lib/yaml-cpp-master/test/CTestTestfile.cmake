1e9c24c7deeb5cf7d63c9dab8f3ef8ad708897f1
Akendo <akendo@akendo.eu>
2016-03-25 14:13:19 +0100

    Clean up build environment

--- build/lib/yaml-cpp-master/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/The_Dark_Knight/Documents/GitHub/ENetzwerk/lib/yaml-cpp-master/test
-# Build directory: C:/Users/The_Dark_Knight/Documents/GitHub/ENetzwerk/build/lib/yaml-cpp-master/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(yaml-test "/run-tests")
-subdirs(gmock-1.7.0)
--- build/lib/yaml-cpp-master/util/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/The_Dark_Knight/Documents/GitHub/ENetzwerk/lib/yaml-cpp-master/util
-# Build directory: C:/Users/The_Dark_Knight/Documents/GitHub/ENetzwerk/build/lib/yaml-cpp-master/util
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
